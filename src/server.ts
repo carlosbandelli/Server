2f3692cd7ee9f44fb2c2910a1d5d768c88abf64a
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2024-01-16 15:26:12 -0300

    novo

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from '@prisma/client';
-
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
-import * as fs from 'fs';
 
 import { convertHourStringToMinutes } from './utils/convert-minutes-string-to-hour-string';
 import { convertMinutesToHourString } from './utils/convert-hour-string-to-minute';
@@ -75,7 +73,7 @@ app.get('/games/:id/ads', async (request, response) => {
     }
   })
 
-  return response.json(ads.map((ad: { weekDays: string; hourStart: number; hourEnd: number; }) => {
+  return response.json(ads.map(ad => {
     return {
       ...ad,
       weekDays: ad.weekDays.split(','),
@@ -102,28 +100,7 @@ app.get('/ads/:id/discord', async (request, response) => {
   })
 })
 
-const PORT = 3333;
 
-async function startServer() {
-  try {
-    // Verifique a existência do arquivo gerado pelo Prisma
-    const prismaClientExists = fs.existsSync('./prisma/generated/client/index.js');
-
-    if (!prismaClientExists) {
-      console.error('Prisma Client not generated. Run "npm run build" first.');
-      process.exit(1);
-    }
-
-    await prisma.$connect();
-    console.log('Connected to the database');
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    process.exit(1);
-  }
-}
-
-startServer();
+app.listen(5000, () => {
+  console.log(`server listenning port ${5000}`)
+})
